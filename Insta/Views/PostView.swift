db7a114451421d33811f161dd841e3fa0e896f37
Jonathon Buhler <Joonaynay@gmail.com>
2021-08-06 19:52:24 -0600

    Other views

---
+++ Insta/Views/PostView.swift
@@ -0,0 +1,20 @@
+//
+//  PostView.swift
+//  Insta
+//
+//  Created by Forrest Buhler on 8/6/21.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostView()
+    }
+}

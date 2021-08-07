cc93da2659b01baac20fbcb6d618381d426ff5f3
Jonathon Buhler <Joonaynay@gmail.com>
2021-08-06 20:28:46 -0600

    Login

--- Insta/InstaApp.swift
+++ Insta/InstaApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct InstaApp: App {
+    
+    @StateObject var user = User()
+    
     var body: some Scene {
         WindowGroup {
             MainView()
+                .environmentObject(user)
         }
     }
 }

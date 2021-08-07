8ecc756a56ed82b59063691af2df7664ee449941
Jonathon Buhler <Joonaynay@gmail.com>
2021-08-06 19:42:40 -0600

    Main

---
+++ Insta/Views/Main.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Insta
+//
+//  Created by Wendy Buhler on 8/6/21.
+//
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        TabView {
+            
+        }
+    }
+}
+
+
+struct Main_Previews: PreviewProvider {
+    static var previews: some View {
+        Main()
+    }
+}

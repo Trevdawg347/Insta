89769546e73a792067bda29e215605373bcb6fe0
Jonathon Buhler <Joonaynay@gmail.com>
2021-08-06 21:27:21 -0600

    a

--- Insta/Views/LoginView.swift
+++ Insta/Views/LoginView.swift
@@ -69,11 +69,23 @@ struct LoginView: View {
                     
                 } //VStack
             } //ZStack
+
         } //Navigation View
+        .onTapGesture {
+            self.hideKeyboard()
+        }
         .navigationViewStyle(StackNavigationViewStyle())
         .accentColor(.red)
     }
 }
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
+
 struct NewAccountView: View {
     var body: some View {
         Text("Hello, World!")

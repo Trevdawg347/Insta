123a3d1546226cf6bc4ba0c68323944de8f5d771
Trevor <tbuhler347@gmail.com>
2021-08-06 19:21:19 -0600

    Initial Commit

---
+++ Insta/InstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaApp.swift
+//  Insta
+//
+//  Created by Wendy Buhler on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct InstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

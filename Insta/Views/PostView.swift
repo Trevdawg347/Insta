2e95c14bffe0029141cbacc744de9b5af810da1a
Jonathon Buhler <Joonaynay@gmail.com>
2021-08-07 20:29:06 -0600

    PostView

--- Insta/Views/PostView.swift
+++ Insta/Views/PostView.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct PostView: View {
+    
+    @State private var caption = ""
+    @State private var showComments = true
+    @State private var showLikes = true
+    @Environment(\.colorScheme) var currentMode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Rectangle()
+                    .ignoresSafeArea()
+                    .foregroundColor(currentMode == .dark ? Color.black : Color(red: 0.95, green: 0.95, blue: 0.95))
+                VStack {
+                    Form {
+                        Section(header: Text("Image")) {
+                            Button("Select Image...") {
+                                
+                            }
+                            .foregroundColor(currentMode == .dark ? Color.white : Color.black)
+                        }
+                        
+                        
+                        Section(header: Text("Caption")) {
+                            TextEditor(text: $caption)
+                                                    
+                        }
+                        Section(header: Text("Settings")) {
+                            Toggle("Allow Comments", isOn: $showComments)
+                                .toggleStyle(SwitchToggleStyle(tint: Color.red))
+                            Toggle("Show Like Count", isOn: $showLikes)
+                                .toggleStyle(SwitchToggleStyle(tint: Color.red))
+
+                                                    
+                        }
+      
+                    }
+                    
+                    Button("Share") {
+                        
+                    }
+                    .padding(.bottom, 30)
+                }
+                
+            }
+            .navigationTitle("New Post")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        
     }
 }
 

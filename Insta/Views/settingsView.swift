bf2d4c1e3045d6481794523d8cdd8d43696d1141
Trevor <tbuhler347@gmail.com>
2021-08-07 22:43:03 -0600

    jk;

--- Insta/Views/settingsView.swift
+++ Insta/Views/settingsView.swift
@@ -12,13 +12,24 @@ struct settingsView: View {
     @State private var settingsList: [String] = [
         "Change Username", "Help", "Logout"
     ]
+    @State private var selection: String? = ""
+    @State private var tags: String = "tags"
     
     var body: some View {
         List {
+            NavigationLink(destination: Text("New"), tag: tags, selection: $selection, label: {})
             ForEach(settingsList, id: \.self) { index in
-                Text(index)
+                HStack {
+                    Button(index) {
+                        selection = tags
+                    }
+                    Spacer()
+                    Image(systemName: "arrow.right")
+                }
             }
-        }.navigationTitle("Settings")
+        }
+        .navigationTitle("Settings")
+        .listStyle(GroupedListStyle())
     }
 }
 

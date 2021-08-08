e53308bca920753dcff4890658d7e7abe746934f
Trevor <tbuhler347@gmail.com>
2021-08-07 21:06:06 -0600

    jkl;

--- Insta/Views/ProfileView.swift
+++ Insta/Views/ProfileView.swift
@@ -10,9 +10,16 @@ import SwiftUI
 struct ProfileView: View {
     
     @EnvironmentObject var user: User
-    @State var followers: Int = 0
-    @State var following: Int = 0
+    @State private var followers: Int = 0
+    @State private var following: Int = 0
+    @State private var selection: String? = ""
+    @State private var tags: String = "tag"
     
+    let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: nil, alignment: nil),
+        GridItem(.flexible(), spacing: nil, alignment: nil),
+        GridItem(.flexible(), spacing: nil, alignment: nil),
+    ]
     var body: some View {
         ZStack {
             VStack {
@@ -49,21 +56,41 @@ struct ProfileView: View {
                     Spacer()
                     Image(systemName: "gearshape.fill")
                         .resizable()
-                        .frame(width: 50, height: 50)
+                        .frame(width: 35, height: 35)
                 }.padding()
-                HStack {
-                    Button("Follow") {
-                        
+                NavigationLink(destination: Text("New Screen"), tag: tags, selection: $selection, label: {})
+                Button(action: {
+                    selection = tags
+                }, label: {
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 200, height: 50)
+                            .cornerRadius(15)
+                        Text("Edit Profile").foregroundColor(.white)
+                            
+                    }
+                })
+                Spacer()
+                ScrollView {
+                    LazyVGrid(columns: columns) {
+                        ForEach(0..<51) { index in
+                            Rectangle()
+                                .frame(height: 150)
+                            
+                        }
                     }
                 }
-                Spacer()
             }
-        }
+            
+        }.navigationBarHidden(true)
     }
 }
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileView().preferredColorScheme(.dark)
+        NavigationView {
+            ProfileView().preferredColorScheme(.dark)
+        }
+        
     }
 }

3216f5e80bd63308c853bb68ec19f8f3a0effbbd
Trevor <tbuhler347@gmail.com>
2021-08-07 20:29:04 -0600

    jkl;

--- Insta/Views/ProfileView.swift
+++ Insta/Views/ProfileView.swift
@@ -10,11 +10,53 @@ import SwiftUI
 struct ProfileView: View {
     
     @EnvironmentObject var user: User
+    @State var followers: Int = 0
+    @State var following: Int = 0
     
     var body: some View {
         ZStack {
-            Button("Sign Out") {
-                user.isSignedIn = false
+            VStack {
+                HStack(alignment: .top) {
+                    ZStack(alignment: .bottomTrailing) {
+                        Image(systemName: "plus.circle.fill")
+                            .foregroundColor(.blue)
+                        Image(systemName: "person.circle")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                    }
+                    Spacer()
+                    VStack {
+                        Button("Followers") {
+                            user.isSignedIn = false
+                        }
+                        .foregroundColor(.red)
+                        Button(String(followers)) {
+                            user.isSignedIn = false
+                        }
+                        .foregroundColor(.red)
+                    }
+                    Spacer()
+                    VStack {
+                        Button("Followers") {
+                            user.isSignedIn = false
+                        }
+                        .foregroundColor(.red)
+                        Button(String(followers)) {
+                            user.isSignedIn = false
+                        }
+                        .foregroundColor(.red)
+                    }
+                    Spacer()
+                    Image(systemName: "gearshape.fill")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }.padding()
+                HStack {
+                    Button("Follow") {
+                        
+                    }
+                }
+                Spacer()
             }
         }
     }
@@ -22,6 +64,6 @@ struct ProfileView: View {
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileView()
+        ProfileView().preferredColorScheme(.dark)
     }
 }

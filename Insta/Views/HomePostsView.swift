158a87ba8d79b8aa5d1a886f7367e401dda3fc61
Jonathon Buhler <Joonaynay@gmail.com>
2021-08-09 01:07:52 -0600

    Editing Home View

---
+++ Insta/Views/HomePostsView.swift
@@ -0,0 +1,44 @@
+//
+//  HomePostsView.swift
+//  Insta
+//
+//  Created by Forrest Buhler on 8/9/21.
+//
+
+import SwiftUI
+
+struct HomePostsView: View {
+    
+    @EnvironmentObject var user: User
+    @State var post: Posts
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                ZStack {
+                    HStack {
+                        Image(systemName: "person.circle")
+                            .foregroundColor(.red)
+                        Text(user.username)
+                    }
+                    Image(uiImage: post.image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: geometry.size.width, height: geometry.size.width)
+                        .clipped()
+                        .onTapGesture {
+                            post.caption = "a"
+                        }
+                    
+                    HStack {
+                        Image(systemName: "heart")
+                        if post.showComments { Image(systemName: "bubble.left") }
+                        if post.showLikes { Text("0 Likes") }
+                        if post.showComments { Text("0 Comments") }
+                    }
+                    if post.caption != "" { Text(post.caption) }
+                }
+            }.frame(width: geometry.size.width, height: geometry.size.width)
+        }
+    }
+}
--- Insta/Views/HomeView.swift
+++ Insta/Views/HomeView.swift
@@ -15,7 +15,7 @@ struct HomeView: View {
     init() {
         UITableView.appearance().showsVerticalScrollIndicator = false
     }
-
+    
     
     var body: some View {
         NavigationView {
@@ -40,66 +40,36 @@ struct HomeView: View {
                                     .frame(width: 30, height: 30)
                                     .foregroundColor(currentMode == .dark ? Color.white : Color.black)
                             })
-                            
                         }
                         .padding(.top)
                         .padding(.horizontal)
                         
-                        List() {
-                            Section(header: Text("Stories")) {
-                                ScrollView(.horizontal, showsIndicators: false) {
-                                    HStack {
-                                        ForEach(0..<10) { users in
-                                            Image(systemName: "person.circle")
-                                                .resizable()
-                                                .scaledToFill()
-                                                .frame(width: 60, height: 60)
-                                                .padding(.leading)
-                                                .padding(.vertical, 5)
-                                                .foregroundColor(currentMode == .dark ? Color.white : Color.black)
-                                        }
+                        ScrollView() {
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack {
+                                    ForEach(0..<10) { users in
+                                        Image(systemName: "person.circle")
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 60, height: 60)
+                                            .padding(.leading)
+                                            .padding(.vertical, 5)
+                                            .foregroundColor(currentMode == .dark ? Color.white : Color.black)
                                     }
                                 }
                             }
+                            
+                            
                             if !user.posts.isEmpty {
-                                Section(header: Text("Posts")) {
-                                    ForEach(user.posts.reversed()) { post in
-                                        HStack {
-                                            Image(systemName: "person.circle")
-                                                .foregroundColor(.red)
-                                            Text(user.username)
-                                        }
-                                        Image(uiImage: post.image)
-                                            .resizable()
-                                            .scaledToFill()
-                                            .frame(width: geometry.size.width - 30, height: geometry.size.width - 30)
-                                            .clipped()
-                                        
-                                        
-                                        
-                                        
-                                        
-                                        
-                                        HStack {
-                                            Image(systemName: "heart")
-                                            if post.showComments {
-                                                Image(systemName: "bubble.left")
-                                            }
-                                            if post.showLikes {
-                                                Text("0 Likes")
-                                            }
-                                            if post.showComments {
-                                                Text("0 Comments")
-                                            }
-                                        }
-                                        if post.caption != "" {
-                                            Text(post.caption)
-                                        }
-                                    }
+                                
+                                ForEach(user.posts.reversed()) { post in
+                                    
+                                    HomePostsView(post: post)
+                                        .frame(width: geometry.size.width, height: geometry.size.width)
+                                    
                                 }
                             }
                         }
-                        .listStyle(GroupedListStyle())
                     }
                     Rectangle()
                         .foregroundColor(Color.gray.opacity(0.4))
